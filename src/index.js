abc10dfe8405f9f388ef495149b2ac41ad3a2b71
haithem <haithem.amri.extracadabra@gmail.com>
2019-09-11 10:35:14 +0200

    ft(#add-handleCardAction):add handleCardAction() method

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import './lib'
-import { Stripe, baseStyle } from './stripeElements'
-import StripeElement from './StripeElement'
-import Card from './Card'
-import CardExpiry from './CardExpiry'
-import CardCvc from './CardCvc'
-import CardNumber from './CardNumber'
-import PostalCode from './PostalCode'
+import "./lib";
+import { Stripe, baseStyle } from "./stripeElements";
+import StripeElement from "./StripeElement";
+import Card from "./Card";
+import CardExpiry from "./CardExpiry";
+import CardCvc from "./CardCvc";
+import CardNumber from "./CardNumber";
+import PostalCode from "./PostalCode";
 
 module.exports = {
   Card,
@@ -15,15 +15,41 @@ module.exports = {
   PostalCode,
   StripeElement,
   baseStyle,
-  get instance() { return Stripe.instance },
-  get createToken() { return Stripe.createToken },
-  get createSource() { return Stripe.createSource },
-  get retrieveSource() { return Stripe.retrieveSource },
-  get paymentRequest() { return Stripe.paymentRequest },
-  get redirectToCheckout() { return Stripe.redirectToCheckout },
-  get retrievePaymentIntent() { return Stripe.retrievePaymentIntent },
-  get handleCardPayment() { return Stripe.handleCardPayment },
-  get handleCardSetup() { return Stripe.handleCardSetup },
-  get confirmPaymentIntent() { return Stripe.confirmPaymentIntent },
-  get createPaymentMethod() { return Stripe.createPaymentMethod }
-}
+  get instance() {
+    return Stripe.instance;
+  },
+  get createToken() {
+    return Stripe.createToken;
+  },
+  get createSource() {
+    return Stripe.createSource;
+  },
+  get retrieveSource() {
+    return Stripe.retrieveSource;
+  },
+  get paymentRequest() {
+    return Stripe.paymentRequest;
+  },
+  get redirectToCheckout() {
+    return Stripe.redirectToCheckout;
+  },
+  get retrievePaymentIntent() {
+    return Stripe.retrievePaymentIntent;
+  },
+  get handleCardPayment() {
+    return Stripe.handleCardPayment;
+  },
+  get handleCardSetup() {
+    return Stripe.handleCardSetup;
+  },
+  get handleCardAction() {
+    return Stripe.handleCardAction;
+  },
+
+  get confirmPaymentIntent() {
+    return Stripe.confirmPaymentIntent;
+  },
+  get createPaymentMethod() {
+    return Stripe.createPaymentMethod;
+  }
+};
